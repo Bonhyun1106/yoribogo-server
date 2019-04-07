cd7a6bbfa391d818ec85e74182881adecf4a48d1
Woojoong Kim <44341196+kwj3682@users.noreply.github.com>
2019-04-08 00:38:26 +0900

    signup build

--- yoribogo/src/main/java/kr/co/yoribogo/repository/dao/SignupMapper.java
+++ yoribogo/src/main/java/kr/co/yoribogo/repository/dao/SignupMapper.java
@@ -1,6 +1,9 @@
 package kr.co.yoribogo.repository.dao;
 
+import kr.co.yoribogo.repository.vo.MemberVO;
+
 public interface SignupMapper {
 	public int getMemIdOnly(String id);
 	public int getMemEmailOnly(String email);
+	public void insertMember(MemberVO member);
 }
