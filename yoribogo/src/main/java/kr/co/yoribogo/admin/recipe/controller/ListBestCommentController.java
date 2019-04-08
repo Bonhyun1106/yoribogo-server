2d703616020359c588682351f797b160a4cc8a7f
Woojoong Kim <44341196+kwj3682@users.noreply.github.com>
2019-04-08 15:02:37 +0900

    merge

--- yoribogo/src/main/java/kr/co/yoribogo/admin/recipe/controller/ListBestCommentController.java
+++ yoribogo/src/main/java/kr/co/yoribogo/admin/recipe/controller/ListBestCommentController.java
@@ -2,14 +2,4 @@ package kr.co.yoribogo.admin.recipe.controller;
 
 public class ListBestCommentController {
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/board/list.do")
-public class AdminListRecipeController extends HttpServlet {
-	
-	
 }
