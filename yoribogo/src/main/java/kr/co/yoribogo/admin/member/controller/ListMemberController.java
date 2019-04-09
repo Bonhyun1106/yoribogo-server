bfdd8ed17c52cd0567047911e4c483e911a779a8
parkjisuzzang <47881123+parkjisuzzang@users.noreply.github.com>
2019-04-09 18:17:33 +0900

    지수 전역커밋/listmembercontroller, admin.xml

--- yoribogo/src/main/java/kr/co/yoribogo/admin/member/controller/ListMemberController.java
+++ yoribogo/src/main/java/kr/co/yoribogo/admin/member/controller/ListMemberController.java
@@ -1,6 +1,48 @@
 package kr.co.yoribogo.admin.member.controller;
 
+import java.io.IOException;
 
-public class ListMemberController {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import kr.co.yoribogo.common.db.MyAppSqlConfig;
+import kr.co.yoribogo.repository.dao.AdminMapper;
+import kr.co.yoribogo.repository.vo.PageVO;
+
+@WebServlet("/admin/recipe/listmember.do")
+public class ListMemberController extends HttpServlet  {
+
+private AdminMapper mapper;
+	
+
+	public ListMemberController() {
+		mapper = MyAppSqlConfig.getSqlSession().getMapper(AdminMapper.class);
+	}
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+	 
+		PageVO page = new PageVO();
+		int pageNo = 1;
+		try {
+			pageNo = Integer.parseInt(
+					request.getParameter("pageNo"));
+			page.setPageNo(pageNo);
+		} catch (Exception e) {}
+		
+		
+		// 게시물 목록 가져오기
+		request.setAttribute("recipe", mapper.selectListRecipe(page));
+//		request.setAttribute("pageResult", new PageResult(
+//				pageNo, mapper.selectBoardCount()
+//		));
+//		
+		
+		request.getRequestDispatcher("listmember.jsp")
+	       .forward(request, response);
+		
+		
+	}	
 }
