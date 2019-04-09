95d6a79f01a885f4876767e86fb935e2f17b6b24
Woojoong Kim <44341196+kwj3682@users.noreply.github.com>
2019-04-09 15:17:53 +0900

    LoginMapper

--- yoribogo/src/main/java/kr/co/yoribogo/repository/dao/LoginMapper.java
+++ yoribogo/src/main/java/kr/co/yoribogo/repository/dao/LoginMapper.java
@@ -1,9 +1,13 @@
 package kr.co.yoribogo.repository.dao;
 
+import java.util.List;
+
 import kr.co.yoribogo.repository.vo.MemberVO;
+import kr.co.yoribogo.repository.vo.RecipeVO;
 
 public interface LoginMapper {
 	
 	MemberVO selectLogin(MemberVO member);
-	
+	List<RecipeVO> selectRecipeByUser(int memNo);
+	//로그인한 계정의 등록 레시피
 }
