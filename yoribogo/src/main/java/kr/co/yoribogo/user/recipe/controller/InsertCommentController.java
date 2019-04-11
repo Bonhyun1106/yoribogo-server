367198f7a3ae9b6f06baa206a62714130f6246d7
Bonhyun1106 <47881250+Bonhyun1106@users.noreply.github.com>
2019-04-11 12:10:49 +0900

    본현 오전 - 댓글

--- yoribogo/src/main/java/kr/co/yoribogo/user/recipe/controller/InsertCommentController.java
+++ yoribogo/src/main/java/kr/co/yoribogo/user/recipe/controller/InsertCommentController.java
@@ -1,5 +1,43 @@
 package kr.co.yoribogo.user.recipe.controller;
 
-public class InsertCommentController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import kr.co.yoribogo.common.db.MyAppSqlConfig;
+import kr.co.yoribogo.repository.dao.RecipeMapper;
+import kr.co.yoribogo.repository.vo.CommentVO;
+
+@WebServlet("/recipe/insertcomment.do")
+public class InsertCommentController extends HttpServlet{
+	private RecipeMapper mapper;
+	public InsertCommentController() {
+		mapper = MyAppSqlConfig.getSqlSession().getMapper(RecipeMapper.class);
+	}
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8"); 
+		
+		System.out.println("---- insert comment 호출 ---- ");
+//		System.out.println(request.getParameter("no"));
+		System.out.println("!!!!!!!!!!!!!!!!!!" + request.getParameter("commContent"));
+		
+		CommentVO comm = new CommentVO();
+//		comm.setNo(Integer.parseInt(request.getParameter("no")));
+		comm.setNo(42);	// 가데이터  글번호42
+//		comm.setMemNo(Integer.parseInt(request.getParameter("memNo")));
+		comm.setMemNo(1);	// 가데이터 회원번호 1
+		comm.setCommentContent(request.getParameter("commContent"));
+		mapper.insertComment(comm);
+		
+//		PrintWriter out = response.getWriter();
+//		out.println(new Gson().toJson("성공");
+//		out.close();
+	}
 }
--- yoribogo/src/main/java/kr/co/yoribogo/user/recipe/controller/ListCommentController.java
+++ yoribogo/src/main/java/kr/co/yoribogo/user/recipe/controller/ListCommentController.java
@@ -1,5 +1,37 @@
 package kr.co.yoribogo.user.recipe.controller;
 
-public class ListCommentController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import kr.co.yoribogo.common.db.MyAppSqlConfig;
+import kr.co.yoribogo.repository.dao.RecipeMapper;
+
+@WebServlet("/recipe/list-comment.do")
+public class ListCommentController extends HttpServlet{
+	private RecipeMapper mapper;
+	public ListCommentController() {
+		mapper = MyAppSqlConfig.getSqlSession().getMapper(RecipeMapper.class);
+	}
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		
+		System.out.println(" ------ comment 호출 ------ ");
+		int no = Integer.parseInt(request.getParameter("no"));
+//		System.out.println("Comment no : " + no);
+//		System.out.println("Comment DB" + mapper.selectComment(no));
+		
+		
+		PrintWriter out = response.getWriter();
+		out.println(new Gson().toJson(mapper.selectComment(no)));
+		out.close();
+	}
 }
