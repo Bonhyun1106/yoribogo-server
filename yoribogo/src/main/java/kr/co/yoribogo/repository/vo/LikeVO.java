492463cddab151d374543d523d420e6812e9f2ce
Bonhyun1106 <47881250+Bonhyun1106@users.noreply.github.com>
2019-04-11 16:14:49 +0900

    LikeVO

---
+++ yoribogo/src/main/java/kr/co/yoribogo/repository/vo/LikeVO.java
@@ -0,0 +1,31 @@
+package kr.co.yoribogo.repository.vo;
+
+public class LikeVO {
+	private int likeNo;
+	private int memNo;
+	private int recipeNo;
+	//ujjjjjjj
+	
+	public int getLikeNo() {
+		return likeNo;
+	}
+	public void setLikeNo(int likeNo) {
+		this.likeNo = likeNo;
+	}
+	public int getMemNo() {
+		return memNo;
+	}
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+	public int getRecipeNo() {
+		return recipeNo;
+	}
+	public void setRecipeNo(int recipeNo) {
+		this.recipeNo = recipeNo;
+	}
+	
+	
+	
+
+}
